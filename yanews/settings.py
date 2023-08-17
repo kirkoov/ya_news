eb6c6832cd9c1e1e7038519bc6691992c4a941fc
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-17 23:02:01 +0300

    A line too long in the settings rectified.

--- yanews/settings.py
+++ yanews/settings.py
@@ -4,7 +4,9 @@ from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-7)dgs++2!#==aye4rd=5)c)bw0eokiyqx0hts6#t80!$c&$s+('
+SECRET_KEY = (
+    'django-insecure-7)dgs++2!#==aye4rd=5)c)bw0eokiyqx0hts6#t80!$c&$s+('
+)
 
 DEBUG = True
 
