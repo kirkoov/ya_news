0e39efc81a7c48f96c00e07c89293e42adb0a029
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-23 07:01:15 +0300

    Saving the state b4 the lesson HW.

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -47,3 +47,25 @@ class TestRoutes(TestCase):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_comment_edit_and_delete(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in ('news:edit', 'news:delete'):
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=(self.comment.id,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_redirect_for_anonymous_client(self):
+        login_url = reverse('users:login')
+        for name in ('news:edit', 'news:delete'):
+            with self.subTest(name=name):
+                url = reverse(name, args=(self.comment.id,))
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
