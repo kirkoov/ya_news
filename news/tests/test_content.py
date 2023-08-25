a98acf1d31b74e7ad22d58a0069f2c49693f8c6b
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-25 18:33:07 +0300

    News order on the home page: tested.

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
@@ -10,9 +12,14 @@ class TestHomePage(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        today = datetime.today()
         all_news = []
         for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
-            news = News(title=f'Новость {index}', text='Просто текст.')
+            news = News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                date=today - timedelta(days=index)
+            )
             all_news.append(news)
         News.objects.bulk_create(all_news)
 
@@ -24,3 +31,12 @@ class TestHomePage(TestCase):
             len(response.context['news_list']),
             settings.NEWS_COUNT_ON_HOME_PAGE
         )
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        news_list = response.context['news_list']
+        all_dates = [news.date for news in news_list]
+        # Сортируем полученный список по убыванию.
+        sorted_dates = sorted(all_dates, reverse=True)
+        # Проверяем, что исходный список был отсортирован правильно.
+        self.assertEqual(all_dates, sorted_dates)
