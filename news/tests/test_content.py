2b0e851fd061d1595a0d0cb4fe27840226fa16a6
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-25 19:00:33 +0300

    Comments tested for their order: earlier ones come first.

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timedelta
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
-from news.models import News
+from news.models import Comment, News
+
+
+User = get_user_model()
 
 
 class TestHomePage(TestCase):
@@ -40,3 +45,34 @@ class TestHomePage(TestCase):
         sorted_dates = sorted(all_dates, reverse=True)
         # Проверяем, что исходный список был отсортирован правильно.
         self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        now = timezone.now()
+        for index in range(2):
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            comment.created = now + timedelta(days=index)
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        # Проверяем, что объект новости находится в словаре контекста
+        # под ожидаемым именем - названием модели.
+        self.assertIn('news', response.context)
+        # Получаем объект новости.
+        news = response.context['news']
+        # Получаем все комментарии к новости.
+        all_comments = news.comment_set.all()
+        # Проверяем, что время создания первого комментария в списке
+        # меньше, чем время создания второго.
+        self.assertLess(all_comments[0].created, all_comments[1].created)
