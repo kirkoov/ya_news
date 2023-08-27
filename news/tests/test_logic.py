8f82485fcbc95b093815fd799844d966ff8989c3
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-27 12:22:41 +0300

    Carry on with logic testing.

--- news/tests/test_logic.py
+++ news/tests/test_logic.py
@@ -5,7 +5,6 @@ from django.test import Client, TestCase
 from django.urls import reverse
 
 # Импортируем из файла с формами список стоп-слов и предупреждение формы.
-# Загляните в news/forms.py, разберитесь с их назначением.
 from news.forms import BAD_WORDS, WARNING
 from news.models import Comment, News
 
@@ -48,3 +47,59 @@ class TestCommentCreation(TestCase):
         self.assertEqual(comment.text, self.COMMENT_TEXT)
         self.assertEqual(comment.news, self.news)
         self.assertEqual(comment.author, self.user)
+
+    def test_user_cant_use_bad_words(self):
+        # Формируем данные для отправки формы; текст включает
+        # первое слово из списка стоп-слов.
+        bad_words_data = {'text': f'Какой-то текст, {BAD_WORDS[0]}, еще текст'}
+        # Отправляем запрос через авторизованный клиент.
+        response = self.auth_client.post(self.url, data=bad_words_data)
+        # Проверяем, есть ли в ответе ошибка формы.
+        self.assertFormError(
+            response,
+            form='form',
+            field='text',
+            errors=WARNING
+        )
+        # Дополнительно убедимся, что комментарий не был создан.
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 0)
+
+
+class TestCommentEditDelete(TestCase):
+    # Тексты для комментариев не нужно дополнительно создавать
+    # (в отличие от объектов в БД), им не нужны ссылки на self или cls,
+    # поэтому их можно перечислить просто в атрибутах класса.
+    COMMENT_TEXT = 'Текст комментария'
+    NEW_COMMENT_TEXT = 'Обновлённый комментарий'
+
+    @classmethod
+    def setUpTestData(cls):
+        # Создаём новость в БД.
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+        # Формируем адрес блока с комментариями, который понадобится для тестов
+        news_url = reverse('news:detail', args=(cls.news.id,))  # Адрес новости
+        # Адрес блока с комментариями
+        cls.url_to_comments = news_url + '#comments'
+        # Создаём пользователя - автора комментария.
+        cls.author = User.objects.create(username='Автор комментария')
+        # Создаём клиент для пользователя-автора.
+        cls.author_client = Client()
+        # "Логиним" пользователя в клиенте.
+        cls.author_client.force_login(cls.author)
+        # Делаем всё то же самое для пользователя-читателя.
+        cls.reader = User.objects.create(username='Читатель')
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
+        # Создаём объект комментария.
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text=cls.COMMENT_TEXT
+        )
+        # URL для редактирования комментария.
+        cls.edit_url = reverse('news:edit', args=(cls.comment.id,))
+        # URL для удаления комментария.
+        cls.delete_url = reverse('news:delete', args=(cls.comment.id,))
+        # Формируем данные для POST-запроса по обновлению комментария.
+        cls.form_data = {'text': cls.NEW_COMMENT_TEXT}
